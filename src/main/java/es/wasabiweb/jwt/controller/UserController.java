5a15f1aabf95a9145ed0e0cfeed15abca8792a3e
Alvaro G. Almaraz <alvarogonzalezalmaraz@gmail.com>
2021-10-14 23:41:42 +0200

    Git check

--- src/main/java/es/wasabiweb/jwt/controller/UserController.java
+++ src/main/java/es/wasabiweb/jwt/controller/UserController.java
@@ -1,9 +1,10 @@
 package es.wasabiweb.jwt.controller;
 
 import org.springframework.web.bind.annotation.RestController;
-/import es.wasabiweb.jwt.service.AppUserService;
+import es.wasabiweb.jwt.service.AppUserService;
 
 @RestController
+//Prueba git
 public class UserController {
 
     private final AppUserService appUserService;
